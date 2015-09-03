d9f31da1877a860ec3b7d4c3ee1b917c49628fbb
Fitz Elliott <fitz@cos.io>
2015-09-03 11:32:24 -0400

    initial commit (generated by yeoman)

---
+++ app/scripts/controllers/main.js
@@ -0,0 +1,17 @@
+'use strict';
+
+/**
+ * @ngdoc function
+ * @name fileApiTestappApp.controller:MainCtrl
+ * @description
+ * # MainCtrl
+ * Controller of the fileApiTestappApp
+ */
+angular.module('fileApiTestappApp')
+  .controller('MainCtrl', function () {
+    this.awesomeThings = [
+      'HTML5 Boilerplate',
+      'AngularJS',
+      'Karma'
+    ];
+  });
