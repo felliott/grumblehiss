9e11ef6f3e300a8843eb7fcfcf6af95daf448139
Fitz Elliott <fitz@cos.io>
2015-09-04 12:31:44 -0400

    fill out node page, adding hiding/file fetching

--- app/scripts/controllers/node.js
+++ app/scripts/controllers/node.js
@@ -8,8 +8,14 @@
  * Controller of the fileApiTestappApp
  */
 angular.module('fileApiTestappApp')
-  .controller('NodeCtrl', function ($scope, node) {
+  .controller('NodeCtrl', function ($scope, node, Restangular) {
     $scope.thisNode = node;
-
     $scope.nodeFiles = node.getFiles().$object;
+
+    $scope.isFolder = function(file) { return file.attributes.item_type === 'folder'; };
+    $scope.expandFolder = function(folder) {
+      folder.dir_contents = Restangular.allUrl('files/', folder.links.related.href).getList().$object;
+    };
+
+
   });
