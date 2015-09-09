c1c5dea73bbe3f6050301d0bbfbe2c97f9abacfb
Fitz Elliott <fitz@cos.io>
2015-09-09 11:39:20 -0400

    remove unused code from Files service
    All of this was moved to node.js, since I don't currently know how to
    wrap the nodelist in the Files service.

--- app/scripts/services/files.js
+++ app/scripts/services/files.js
@@ -58,58 +58,6 @@ services.factory(
      });
 
      Restangular.extendModel(serviceName, function(model) {
-
-       // GET
-       model.download = function() { // file
-         return model.customGET(model.links.download);
-       };
-
-       // PUT
-       model.createSubfolder = function() { // folder-only
-         return model.customPUT(model.links.new_folder);
-       };
-       model.uploadFile = function() { // folder-only
-         return model.customPUT(model.links.upload);
-       };
-       model.updateFile = function() { // file
-         return model.customPUT(model.links.upload);
-       };
-       // model.rename = function() {  // file+folder
-       //   return model.put(model.links.upload);
-       // };
-
-       // POST
-       model.moveTo = function() { // file+folder
-         return model.customPOST(model.links.move, {
-           action: 'move', path: 'path', rename: 'rename',
-           conflict: true
-         });
-       };
-       model.copyTo = function() { // file+folder
-         return model.customPOST(model.links.move, {
-           action: 'copy', path: 'path', rename: 'rename',
-           conflict: true
-         });
-       };
-
-       // DELETE
-       model.delete = function() {  // file
-         if (model.attributes.type === 'folder') {
-           throw 'delete() is not valid for a folder. Perhaps you wanted rmTree()?';
-         }
-         return model.remove();
-       };
-       model.rmTree = function() { // folder
-         if (model.attributes.type === 'file') {
-           throw 'rmTree() is not valid for a file. Perhaps you wanted delete()?';
-         }
-         return model.remove();
-       };
-
-
-
-       model.viewVersions = function() {};
-
        return model;
      });
 
